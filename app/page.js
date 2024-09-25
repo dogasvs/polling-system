f5d262becb8a6de36b93a880e9ef2c83a12022b9
Doğa Savaş <doasavaas@gmail.com>
2024-09-25 23:40:19 +0300

    home sayfasına lottie eklendi

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+"use client"
+import Lottie from "lottie-react";
+import Animation from "@/lotties/Animation.json"
+
 export default function Home() {
   return (
-  <div className="container">
-  
+  <div className="home">
+    <h1>Yoklama Takip Sistemi</h1>
+    <Lottie animationData={Animation} width={300} height={300} loop={true} />
   </div>
   );
 }
