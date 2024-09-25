f9987f07b05199f4735c52f450679710474b68cb
Doğa Savaş <doasavaas@gmail.com>
2024-09-26 01:48:46 +0300

    student detail sayfası css eklendi

--- app/Students/[id]/page.jsx
+++ app/Students/[id]/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import ClassData from "@/data.json"
 
 export default async function StudentDetail({ params }) {
-  const {id} = params;
+  const { id } = params;
 
   const ogrenci = ClassData.find(x => x.id == id);
   console.log(ogrenci);
@@ -10,28 +10,36 @@ export default async function StudentDetail({ params }) {
   return (
     <div className="studentDetail">
       <Link href="/Students">Geri</Link>
-      <h1>{ogrenci.name}</h1>
-      <p>Öğrenci Numarası: {ogrenci.studentNumber}</p>
-      <p>Sınıf: {ogrenci.class}</p>
+      <div className="studentImage">
+        <img src="/img/user.jpeg" alt="Öğrenci Resmi" />
+      </div>
+      <div className="studentInfo">
+        <div className="studentInfoGenel">
+        <h1>{ogrenci.name}</h1>
+        <p> <strong>Öğrenci Numarası:  </strong> {ogrenci.studentNumber}</p>
+        <p>Sınıf: {ogrenci.class}</p>
+        </div>
 
-      <div className="student-details">
-        <h2>Detaylar</h2>
-        <p>Not: {ogrenci.details.grade}</p>
-        <p>Devamlılık: {ogrenci.details.attendance}</p>
-        <p>Açıklamalar: {ogrenci.details.notes}</p>
+        <div className="studentInfoDetail">
+          <h2>Detaylar</h2>
+          <p> <strong>Not: </strong> {ogrenci.details.grade}</p>
+          <p> <strong>Devamlılık:  </strong> {ogrenci.details.attendance}</p>
+          <p> <strong>  Açıklamalar:</strong> {ogrenci.details.notes}</p>
 
-        <h3>İletişim Bilgileri</h3>
-        <p>Email: {ogrenci.details.contact.email}</p>
-        <p>Telefon: {ogrenci.details.contact.phone}</p>
-        <p>Adres: {ogrenci.details.contact.address}</p>
+          <h3>İletişim Bilgileri</h3>
+          <p> <strong> Email:  </strong> {ogrenci.details.contact.email}</p>
+          <p><strong>Telefon:  </strong> {ogrenci.details.contact.phone}</p>
+          <p> <strong> Adres: </strong>{ogrenci.details.contact.address}</p>
 
-        <h3>Ekstradan Katıldığı Aktiviteler</h3>
-        <ul>
-          {ogrenci.details.extraActivities.map((activity, index) => (
-            <li key={index}>{activity}</li>
-          ))}
-        </ul>
+          <h3>Ekstradan Katıldığı Aktiviteler</h3>
+          <ul>
+            {ogrenci.details.extraActivities.map((activity, index) => (
+              <li key={index}>{activity}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
+
   )
 }
